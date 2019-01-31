a545432e8fbfccb12250cafb15e714c35523da2e
Zhandos Nurakhmetov <zhandos.nurakhmetov@mail.ru>
2019-01-31 18:22:36 +0600

    first commit

---
+++ FastlaneTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneTest
+//
+//  Created by Жандос НУРАХМЕТОВ on 1/31/19.
+//  Copyright © 2019 Nurakhmetov Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
---
+++ FastlaneTestTests/FastlaneTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  FastlaneTestTests.swift
+//  FastlaneTestTests
+//
+//  Created by Жандос НУРАХМЕТОВ on 1/31/19.
+//  Copyright © 2019 Nurakhmetov Inc. All rights reserved.
+//
+
+import XCTest
+@testable import FastlaneTest
+
+class FastlaneTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
